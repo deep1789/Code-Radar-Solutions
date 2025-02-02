d22b80a8f8fd28f5c9dec596e876602ce74ef886
deep1789 <42023574+deep1789@users.noreply.github.com>
2025-02-02 20:54:35 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,38 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    int x, y;
-    char ch;
-    
-    // Reading inputs
-    scanf("%d%d", &x, &y);
-    scanf(" %c", &ch);  // Fixed input issue by adding a space before %c
-    
-    // Displaying the operator
-    printf("%c\n", ch);
 
-    // Performing operations based on the operator
-    switch (ch) {
-        case '+':
-            printf("%d\n", x + y);
-            break;
-        case '-':
-            printf("%d\n", x - y);
-            break;
-        case '*':
-            printf("%d\n", x * y);
-            break;
-        case '/':
-            // Avoid division by zero
-            if (y != 0) {
-                printf("%d\n", x / y);
-            } else {
-                printf("Division by zero is not allowed.\n");
-            }
-            break;
-        default:
-            printf("Invalid operator\n");
+int main() {
+    int x,y;
+    char ch;
+    scanf("%d%d",&x,&y);
+    scanf(" %c",&ch);
+    printf("%c",ch);
+
     }
 
-    return 0;
-}
+
+    
