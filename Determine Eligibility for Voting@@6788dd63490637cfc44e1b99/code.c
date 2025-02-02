9b286ece7d7578bcefd1af75e35162aeaf9cc3c8
deep1789 <42023574+deep1789@users.noreply.github.com>
2025-02-02 19:53:40 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int age;
+
+    scanf("%d",&age);
+
+    if(age>18)
+    {
+        printf("Elogible");
+    }
+    else
+    {
+        printf("Not Eligible");
+    }
+
+    return 0;
+}
